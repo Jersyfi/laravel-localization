18d9a82a79b4e02f1a22d4e599cf4c14b4f56db6
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2021-02-17 13:30:31 +0100

    Delete Exceptions from registration

---
+++ Localization.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jersyfi\Localization;
+
+use Jersyfi\Localization\HasExceptions;
+
+class Localization
+{
+    use HasExceptions;
+    
+    public function get()
+    {
+        return "alles richtig gemacht junge";
+    }
+}
