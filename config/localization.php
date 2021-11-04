63835ad1cbfc405de18165b57b26d0d74fa3caa1
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2021-11-04 12:32:30 +0100

    Update localization.php

--- config/localization.php
+++ config/localization.php
@@ -27,19 +27,5 @@ return [
      * When no locale is stored in session or in auth users database table user gets redirected
      */
     'detect_locale' => false,
-    
-    /**
-     * Application can store the prefered_locale in the users database table
-     */
-    'store_users_database' => true,
-    
-    /**
-     * Setup for the users database table
-     * Only if 'store_users_database' is set to true
-     */
-    'database' => [
-        'users_table_name' => 'users',
-        'prefered_locale_column_name' => 'prefered_locale',
-    ],
 
 ];
